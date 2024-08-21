5e74ec89f1372b10a563ba5f5f9ce45890e8cffe
Mehdi LAHAK <mehdi.lahak@oumdin.com>
2024-08-21 13:48:59 +0100

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,11 @@ public class Main {
         Random ran = new Random();
         Scanner sc = new Scanner(System.in);
 
-        while(true) {
+        int playerScore = 0;
+        int computerScore = 0;
+
+        while (playerScore < 3 && computerScore < 3) {
+
             System.out.println("1 - Rock"
                     + "\n" + "2 - Paper"
                     + "\n" + "3 - Sissor" + "\n" + "Chose one: ");
@@ -22,26 +26,43 @@ public class Main {
             if (player == 1 && computer == 2) {
                 System.out.println("Player chose: Rock" + "\n" + "Computer chose: Paper");
                 System.out.println("Computer win's");
+                computerScore++;
+                System.out.println("Player" + playerScore + " || " + " Computer " + computerScore);
             }
             if (player == 2 && computer == 1) {
                 System.out.println("Player chose: Paper" + "\n" + "Computer chose: Rock");
                 System.out.println("Player win's");
+                playerScore++;
+                System.out.println("Player " + playerScore + " || " + " Computer " + computerScore);
+
             }
             if (player == 3 && computer == 1) {
                 System.out.println("Player chose: Scissor" + "\n" + "Computer chose: Rock");
                 System.out.println("Computer win's");
+                computerScore++;
+                System.out.println("Player " + playerScore + " || " + " Computer" + computerScore);
+
             }
             if (player == 1 && computer == 3) {
                 System.out.println("Player chose: Rock" + "\n" + "Computer chose: Sissor");
                 System.out.println("Player win's");
+                playerScore++;
+                System.out.println("Player " + playerScore + " || " + " Computer" + computerScore);
+
             }
             if (player == 3 && computer == 2) {
                 System.out.println("Player chose: Sissor" + "\n" + "Computer chose: Paper");
                 System.out.println("Player win's");
+                playerScore++;
+                System.out.println("Player " + playerScore + " || " + " Computer" + computerScore);
+
             }
             if (player == 2 && computer == 3) {
                 System.out.println("Player chose: Paper" + "\n" + "Computer chose: Sissor");
                 System.out.println("Computer win's");
+                computerScore++;
+                System.out.println("Player " + playerScore + " || " + " Computer" + computerScore);
+
             }
             if(player > 3){
                 System.out.println("Chose between the 3 Choices");
@@ -55,7 +76,13 @@ public class Main {
             System.out.println("---------------------------------------------");
         }
 
-
+        if(playerScore > computerScore){
+            System.out.println("'Player' Win! the Round with a Score of: " + playerScore + "||" + computerScore);
+            System.out.println("-------------------------------------------");
+        }else{
+            System.out.println("'Computer' Win! the Round with a Score of: " + computerScore + "||" + playerScore);
+            System.out.println("----------------------------------------------");
+        }
 
 
 
